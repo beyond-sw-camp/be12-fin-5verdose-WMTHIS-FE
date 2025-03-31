13a705404af4eb319b2a734034362ac7c07bf112
seungjae <dhtmdwo73@naver.com>
2025-03-31 16:48:30 +0900

    [feat] Header 페이지 작성

---
+++ src/components/BeforeLogin.vue
@@ -0,0 +1,79 @@
+<script setup>
+// 별도 로직 없음
+</script>
+
+<template>
+  <div class="background">
+    <!-- 왼쪽: 이미지 -->
+    <div class="left_picture">
+      <img src="../../img/image.png" alt="샐러드 이미지" class="background_img" />
+    </div>
+
+    <!-- 오른쪽: 텍스트 + 버튼 -->
+    <div class="right_content">
+      <img src="../../img/icon.png" alt="샐러드 이미지" class="right_logo_img" />
+      <div>
+        <p class="right_letter">자동화된 재고관리부터</p>
+        <h1 class="right_letter right_letter_bold">다~ 되는 WMTHIS POS</h1>
+      </div>
+      <div>
+        <v-btn rounded="xl" class="btn_template btn_font">회원가입</v-btn>
+      </div>
+      <div>
+        <v-btn rounded="xl" class="btn_template btn_font">회원이에요</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+.left_picture {
+  width: 50%;
+}
+.background_img {
+  width: 100%;
+  height: 100%;
+}
+
+.right_content {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.875rem;
+  background-color: #eeeeee;
+}
+
+.right_logo_img {
+  width: 50%;
+  height: 20%;
+}
+
+.right_letter {
+  font-size: 40px;
+}
+
+.right_letter_bold {
+  font-weight: 900;
+}
+
+.btn_font {
+  font-size: 25px;
+  color: white;
+  width: 280px;
+}
+.btn_template {
+  background-color: #666666;
+  display: flex;
+  align-items: center; /* 세로 정렬 */
+  justify-content: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<script setup>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiMenu } from "@mdi/js";
+import { mdiDotsVertical } from "@mdi/js";
+import { mdiBell } from "@mdi/js";
+
+const path1 = mdiMenu;
+const path2 = mdiBell;
+const path3 = mdiDotsVertical;
+</script>
+
+<template>
+  <div class="total_header">
+    <div class="header_up">
+      <svg-icon type="mdi" :path="path1" class="icon_style header_left_icon"></svg-icon>
+      <div class="header_up_write">홈 화면</div>
+      <div class="header_right_icon">
+        <svg-icon type="mdi" :path="path2" class="icon_style"></svg-icon>
+        <svg-icon type="mdi" :path="path3" class="icon_style"></svg-icon>
+      </div>
+    </div>
+    <div class="header_down">
+      <div class="header_down_write">메인페이지</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.total_header {
+  width: 100vw;
+  height: 13vh;
+}
+.header_up {
+  width: 100%;
+  height: 50%;
+  background-color: #666666;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header_left_icon {
+  margin-left: 1.25rem;
+}
+
+.header_up_write {
+  font-size: 1.25rem;
+  color: rgb(221, 224, 227);
+}
+
+.header_right_icon {
+  display: flex;
+  gap: 1.25rem;
+  margin-right: 1.875rem;
+}
+
+.header_down {
+  width: 100%;
+  height: 50%;
+  background-color: #eeeeee;
+}
+
+.header_down_write {
+  font-size: 1.125rem;
+  font-weight: 900;
+  display: flex;
+  width: 15%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 0.25rem solid black;
+}
+
+.icon_style {
+  color: white;
+  width: 1.5rem;
+  height: 1.5rem;
+}
+</style>
