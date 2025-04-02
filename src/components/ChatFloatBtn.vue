c4d4c2da00e1fbe8f3efa86a07110318fdce91e9
scy <tjcksdud0221@naver.com>
2025-04-02 12:05:00 +0900

    컴포넌트 열었을때 아이콘 변경

--- src/components/ChatFloatBtn.vue
+++ src/components/ChatFloatBtn.vue
@@ -8,23 +8,27 @@
   
         <div style="position: fixed; bottom: 20px; right: 20px; z-index: 100;">
           <v-btn fab color="#B1D5C2" @click="toggleComponent">
-            <v-icon color="white">mdi-chat</v-icon>
+            <v-icon color="white" :icon="iconName"></v-icon>
           </v-btn>
   
           <ChatComponent :showComponent="showComponent">
-            </ChatComponent>
+          </ChatComponent>
         </div>
       </v-main>
     </v-app>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
-  import ChatComponent from './ChatComponent.vue';
+  import { ref, computed } from 'vue';
+  import ChatComponent from './ChatComponent.vue'; // ChatComponent 임포트
   
   const showComponent = ref(false);
   
   const toggleComponent = () => {
     showComponent.value = !showComponent.value;
   };
+  
+  const iconName = computed(() => {
+    return showComponent.value ? 'mdi-close' : 'mdi-chat';
+  });
   </script>
