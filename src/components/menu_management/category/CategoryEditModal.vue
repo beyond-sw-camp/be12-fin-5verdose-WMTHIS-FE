fd88dbf5808f13ed6b8f219768a135a51b007b77
scy <tjcksdud0221@naver.com>
2025-04-10 14:38:32 +0900

    모달에 z-index추가중

--- src/components/menu_management/category/CategoryEditModal.vue
+++ src/components/menu_management/category/CategoryEditModal.vue
@@ -28,7 +28,7 @@ const removeOption = (index) => {
 </script>
 
 <template>
-    <div v-if="isOpen" class="modal_overlay" @click.self="emit('close')">
+    <div v-if="isOpen" class="modal_overlay" @click.self="emit('close')" style="z-index: 2000;">
         <div class="modal">
             <div class="modal_content">
                 <div class="modal_header">
