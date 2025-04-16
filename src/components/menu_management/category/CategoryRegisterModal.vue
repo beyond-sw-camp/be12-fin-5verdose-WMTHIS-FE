847ea9e63fe4dff600b3e9e070fde24137fcb85c
scy <tjcksdud0221@naver.com>
2025-04-16 14:15:36 +0900

    카테고리 등록 모달창에 옵션 목록 불러오기

--- src/components/menu_management/category/CategoryRegisterModal.vue
+++ src/components/menu_management/category/CategoryRegisterModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, defineEmits, ref } from 'vue';
+import { defineProps, defineEmits, ref, onMounted } from 'vue';
 import { api } from '@/api'; // API 호출을 위한 axios 인스턴스 import
 
 const props = defineProps({
@@ -10,7 +10,7 @@ const emit = defineEmits(['close']);
 
 const categoryName = ref('');
 const category = ref('');
-const optionList = ['면 추가', '밥 추가', '치즈 추가', '고기 추가'];
+const optionList = ref(['면 추가', '밥 추가', '치즈 추가', '고기 추가']);
 const optionName = ref('');
 const selectedOptions = ref([]);
 
@@ -45,7 +45,21 @@ const removeOption = (index) => {
     selectedOptions.value.splice(index, 1);
 };
 
+// 옵션 리스트 로딩
+const loadOptionList = async () => {
+    const result = await api.getOptionList();
+    if (result) {
+        console.log('옵션 목록:', result);
+        optionList.value = result.content;
+    } else {
+        alert("옵션 목록을 불러오는 데 실패했습니다.");
+    }
+};
 
+onMounted(() => {
+    // 초기화 로직이 필요하다면 여기에 작성
+    loadOptionList();
+});
 </script>
 
 <template>
@@ -75,7 +89,7 @@ const removeOption = (index) => {
                     <div class="ingredient_inputs">
                         <select v-model="optionName">
                             <option value="" disabled selected>옵션 선택</option>
-                            <option v-for="item in optionList" :key="item" :value="item">{{ item }}</option>
+                            <option v-for="item in optionList" :key="item" :value="item">{{ item.name }}</option>
                         </select>
                         <button class="add_btn" @click="addOption">추가</button>
                     </div>
@@ -83,7 +97,7 @@ const removeOption = (index) => {
 
                 <div class="tag_container">
                     <span v-for="(opt, index) in selectedOptions" :key="index" class="tag">
-                        {{ opt }}
+                        {{ opt.name }}
                         <button class="remove_btn" @click="removeOption(index)">✕</button>
                     </span>
                 </div>
