938bad2c64184b5712a21f656442396514ca220e
scy <tjcksdud0221@naver.com>
2025-04-18 12:39:45 +0900

    MarketApi 생성

---
+++ src/api/MarketApi.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "/api", // 백엔드 API 주소로 변경
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // 필요 시 (ex: 쿠키 인증)
+});
