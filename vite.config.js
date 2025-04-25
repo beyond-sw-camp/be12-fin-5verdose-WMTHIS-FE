0b3ee567d5734c9d7ffda0adb7ab1867123e2f2b
taehoon0518 <cjsxogns0518@gmail.com>
2025-04-25 17:49:12 +0900

    [fix] README 수정 및 포트번호 수정

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8081", // 실제 백엔드 주소
+        target: "http://localhost:8080", // 실제 백엔드 주소
         changeOrigin: true,
       },
     },
