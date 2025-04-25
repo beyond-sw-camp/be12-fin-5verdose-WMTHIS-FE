1dafe286a4ce32b173885bdc56c32a9311aa8c1e
scy <tjcksdud0221@naver.com>
2025-04-25 12:35:27 +0900

    로그인 라우터 수정

--- src/components/account_management/Login.vue
+++ src/components/account_management/Login.vue
@@ -36,7 +36,8 @@ const handleLogin = async () => {
         } else {
           localStorage.removeItem("savedEmail");
         }
-        window.location.href = "/";
+
+        router.push({ name: "dashboard" });
       } else {
         errorMessage.value = response.data || "로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.";
       }
